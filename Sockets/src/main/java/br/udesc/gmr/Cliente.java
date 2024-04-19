8b5ad26c6fe8f9021f65d04890f372bd8354b7bd
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2024-04-19 19:36:06 -0300

    v2

--- Sockets/src/main/java/br/udesc/gmr/Cliente.java
+++ Sockets/src/main/java/br/udesc/gmr/Cliente.java
@@ -1,11 +1,21 @@
 package br.udesc.gmr;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Cliente {
-    public static void main(String args[]) throws IOException {
-        var servidor = new Socket("127.0.0.1", 8080);
-        System.out.println("Cliente conectado!");
+    public static void main(String[] args) throws IOException {
+        var cliente = new Socket("127.0.0.1", 8080);
+        var teclado = new Scanner(System.in);
+        var saida = new PrintStream(cliente.getOutputStream());
+        while (teclado.hasNextLine()) {
+            saida.println(teclado.nextLine());
+        }
+
+        saida.close();
+        teclado.close();
+        cliente.close();
     }
 }
--- Sockets/src/main/java/br/udesc/gmr/Servidor.java
+++ Sockets/src/main/java/br/udesc/gmr/Servidor.java
@@ -2,12 +2,23 @@ package br.udesc.gmr;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.Scanner;
 
 public class Servidor {
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         var servidor = new ServerSocket(8080);
-        System.out.println("Servidor iniciado na 8080!");
-        var socket = servidor.accept();
-        System.out.println("Conexão estabelecida!");
+        System.out.println("Servidor iniciado na porta 8080!");
+
+        var cliente = servidor.accept();
+        System.out.println("Cliente " + cliente.getInetAddress().getHostAddress());
+        var saida = new Scanner(cliente.getInputStream());
+
+        while (saida.hasNextLine()) {
+            System.out.println(saida.nextLine());
+        }
+        saida.close();
+        servidor.close();
+        cliente.close();
     }
 }
+
