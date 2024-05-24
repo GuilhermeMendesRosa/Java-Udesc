ad5a0d9e0e0963980c7bca5fe60ef0a7840e071b
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2024-05-24 20:03:59 -0300

    jogo da velha

--- Mobile/app/src/main/java/br/udesc/mobile/MainActivity.java
+++ Mobile/app/src/main/java/br/udesc/mobile/MainActivity.java
@@ -1,52 +1,141 @@
 package br.udesc.mobile;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import br.udesc.mobile.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private boolean playerXTurn = true;
+    private Button[][] buttons = new Button[3][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        GridLayout gridLayout = findViewById(R.id.gridLayout);
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button" + (i * 3 + j + 1);
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this::onButtonClick);
             }
-        });
+        }
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void onButtonClick(View v) {
+        Button button = (Button) v;
+
+        if (!button.getText().toString().equals("")) {
+            return;
+        }
+
+        if (playerXTurn) {
+            button.setText("X");
+            if (checkForWin()) {
+                showToast("Player X wins!");
+                resetBoard();
+                return;
+            } else if (isBoardFull()) {
+                showToast("It's a draw!");
+                resetBoard();
+                return;
+            }
+            playerXTurn = false;
+            computerMove();  // Computer makes a move
+        }
+    }
+
+    private void computerMove() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (buttons[i][j].getText().toString().equals("")) {
+                    buttons[i][j].setText("O");
+                    if (checkForWin()) {
+                        showToast("Player O wins!");
+                        resetBoard();
+                        return;
+                    } else if (isBoardFull()) {
+                        showToast("It's a draw!");
+                        resetBoard();
+                        return;
+                    }
+                    playerXTurn = true;
+                    return;
+                }
+            }
+        }
+    }
+
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")) {
+            return true;
+        }
+        if (field[0][2].equals(field[1][1])
+                && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (buttons[i][j].getText().toString().equals("")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void resetBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+        playerXTurn = true;
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
